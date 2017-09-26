957942f51868dd992d2b7599d173787b70b585cf
zkfbdkf <zkfbdkf@gmail.com>
2017-09-26 11:19:50 +0900

    fetch-search

--- src/app/providers/park-data.ts
+++ src/app/providers/park-data.ts
@@ -23,4 +23,16 @@ export class ParkData {
             return data;
         });
     }
+    getFilteredParks(queryString){
+        return this.load().then(Parks=>{
+            let theFilteredParks: any=[];
+            for(let thePark of Parks){
+                if(thePark.name.toLowerCase().indexOf(queryString.toLowerCase()) > -1){
+                    theFilteredParks.push(thePark);
+                }
+            }
+            return theFilteredParks;
+        });
+
+    }
 }
--- src/pages/park-list/park-list.ts
+++ src/pages/park-list/park-list.ts
@@ -16,20 +16,44 @@ import { ParkData } from '../../app/providers/park-data';
 })
 export class ParkListPage {
   parks: Array<Object> = []
+  searchQuer: string = '';
 
-  constructor(public navCtrl: NavController,  public navParams: NavParams, public parkData: ParkData) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public parkData: ParkData) {
     parkData.getParks().then(theResult => {
       this.parks = theResult;
     })
   }
 
   goParkDetails(theParkData) {
-    this.navCtrl.push("ParkDetailsPage", {parkData: theParkData});
+    this.navCtrl.push("ParkDetailsPage", { parkData: theParkData });
     // console.log(theParkData);
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ParkListPage');
   }
-  
+  getParks(event) {
+    //Reset items back to all of the items
+    this.parkData.getParks().then(theResult => {
+      this.parks = theResult;
+    })
+    //set queryString to the value of the searchbar
+    let queryString = event.target.value;
+    if (queryString !== undefined) {
+      // if the value is an empty string don't filter the items
+      if (queryString.trim() == '') { return; }
+
+      this.parkData.getFilteredParks(queryString).then
+        (theResult => {
+          this.parks = theResult;
+        })
+    }
+  }
+  resetList(event){
+    //Reset items back to all of the items
+    this.parkData.getParks().then(theResult=>{
+      this.parks=theResult;
+    })
+  }
+
 
 }
